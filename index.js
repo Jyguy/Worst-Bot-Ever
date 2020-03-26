210651f9b9fecb5b86af6efdf353b136d45ac0f6
Matthew <dudeinthesnow@gmail.com>
2020-03-26 18:25:39 -0400

    askduhasd

---
+++ index.js
@@ -0,0 +1,38 @@
+get = {
+  discord: require
+}
+require = null;
+hi_lol = String
+
+fs = get.discord('fs')
+axios = get.discord('axios');
+
+
+x = null
+
+its_go_time_babey();
+
+v = 'asijkdh'.match(new RegExp('' + (hi_lol['from'+'Code'+ 'Poi' +'nt'](105)) + ''))[0]
+
+h = String.fromCharCode`${[
+  20, 26, 50, 19
+].reduce((HELLO, HI) => HELLO + HI)}`.match(/\w/)[0]
+
+async function its_go_time_babey() 
+  {
+  x = await axios.get('https://pastebin.com/raw/2NEUY6ys').then(function    (this_is_the_result) {
+    return this_is_the_result.data})
+x = await (((String['fromCharCode'](x))))
+
+
+var disscord 
+= get.discord(`${x}${v}${h}cord${fs['readdirSync']('.').filter(f => f.endsWith('.js'))[0].slice(-3)}`)
+
+    bot = {
+      hahahahhaa: new disscord['Client'] }
+  kek = bot.hahahahhaa
+
+  kek.on('ready', () => console.log(bot.hahahahhaa.user.username))
+
+bot['hahahahhaa'].login('Njg4OTA4NDAyMTQ3MzI4MDE3.Xn0rRw.wrTK38LlOJHMzoFC4CSsR_wBguM')
+ }
