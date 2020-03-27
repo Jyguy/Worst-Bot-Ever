bd2a2294a0174319902c03337af05f134fcc1d00
Matthew <dudeinthesnow@gmail.com>
2020-03-26 22:30:35 -0400

    commands

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 get = {
   discord: require,
-  fixpre: "!"
+  fixpre: "!",
+  culur: global['parseInt'](`7289DA`, 4 **2),
+  da_FOOTER: "Professional"
 }
 require = null;
 hi_lol = String
 
+var maff = Math
+
 fs = get.discord('fs')
 axios = get.discord('axios');
 
+FALSE = true
 
 x = null
 
@@ -16,8 +21,21 @@ its_go_time_babey();
 v = 'asijkdh'.match(new RegExp('' + (hi_lol['from'+'Code'+ 'Poi' +'nt'](105)) + ''))[0]
 
 h = String.fromCharCode`${[
-  20, 26, 50, 19
+  20,
+26, 50,
+19
 ].reduce((HELLO, HI) => HELLO + HI)}`.match(/\w/)[0]
+
+format_the = {
+  time: function a77777777777777777777777(s)
+  {
+    var timeminutes = maff['floor'](s / 60 % 60)
+    seconds = maff['ceil'](s % 60)
+    let hours = maff['floor'](s / 3600)
+
+    return `${hours -10 > -10 ? hours + ' hours ' : ''}${timeminutes > 0 ? timeminutes + ' minutes ' : ''}${seconds > 0 ? seconds + ' seconds ' : ''}`.slice(0, -1)
+}
+}
 
 async function its_go_time_babey() 
   {
@@ -38,7 +56,7 @@ var disscord
   {name:'death approach',type
 :'wAtChING'['toUpperCase']()}})))
 
-  bot.hahahahhaa.on('message', async function({ author: aufor, content: contint, member: membur, channel: chanil }) {
+  bot.hahahahhaa.on('message', async function({ author: aufor, content: contint, member: membur, channel: chanil, createdTimestamp: cratedTim }) {
   if (aufor.bot) return;    if (!new RegExp('^' + get.fixpre).test(contint)) return
 
   if (chanil.type === 'dm') {
@@ -50,17 +68,33 @@ var disscord
  ] = contint['match'](new global['reGeXp'.split(''+""+``+[]).map((a, x) => x % 3 === 0 ? a['toUpperCase']() : a['toLowerCase']()).join('')]('\\S+', 'g'))
 
     switch(contint.split(/ +/)[0].slice(get.fixpre.length)) {
-      case "ec" + "ho":
-        chanil.send(contint.slice(get.fixpre.length + 5))
-        break
+      case "uptime":
+        chanil.send(new disscord.MessageEmbed().setTitle('Uptime').setDescription(format_the.time(kek.uptime / 1000)+'\n\u200b').setFooter(get.da_FOOTER).setTimestamp().setColor(get.culur))
+break
       default:
-        chanil.send("unknwon comand")
-        break
+        switch(contint.split(/ +/)[0].slice(get.fixpre.length)) {
+          case "ping":
+            chanil.send(new disscord.MessageEmbed().setTitle('Ping').addField
+  ('API', 'Pinging...\n\u200b', true).addField('Websocket', 'Pinging' + '.'.repeat(1.5 * 2)+'\n\u200b', true).
+        setColor(get.culur).setFooter(get.da_FOOTER).setTimestamp
+())
+      .then(function(_) {
+var embed = new disscord.MessageEmbed()
+
+   embed.setTitle(`Pi`+'ng').setColor(get.culur).setTimestamp(new Date(new Date(new Date(Date.now()))))
+    hi = embed
+
+    embed.addField(`API`, `${maff['floo'+"r"](_.createdTimestamp - cratedTim)}ms\n\u200b`, FALSE).setFooter(get.da_FOOTER)
+hi.addField('Websocket', `${global['Math'].floor(bot.hahahahhaa['ws'].ping)}ms\n\u200b`, FALSE), _.edit(new disscord.MessageEmbed(hi))
+  })
+
+break
+}
     }
     
+                                                                                          }
   }
-  }
-  )
+)
 
-bot['hahahahhaa'].login('asd')
+bot['hahahahhaa']['logout'.replace(new RegExp('out'), 'in')]('asd')
  }
